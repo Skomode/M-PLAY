cca6fbc730becfc55a56e7004d1d266caae1c9a1
Skomode <lejobass02@gmail.com>
2026-08-11 14:42:20 +0200

    implementacion de las variables de entorno para cloudflare S3

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -1,17 +1,25 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 interface IENV {
-    PORT: string,
-    MONGO_URI : string,
-    JWT_SECRETWORD : string
+  PORT: string;
+  MONGO_URI: string;
+  JWT_SECRETWORD: string;
+  CLOUDFLARE_ACCOUNT_ID: string;
+  R2_ACCESS_KEY_ID: string;
+  R2_SECRET_ACCESS: string;
+  R2_BUCKET_NAME: string;
+  R2_PUBLIC_URL: string;
 }
-
 
 dotenv.config();
 
-export const ENV : IENV = {
-
-    PORT: process.env.PORT || "",
-    MONGO_URI : process.env.MONGO_URI || "",
-    JWT_SECRETWORD : process.env.JWT_SECRETWORD || ""
-}
+export const ENV: IENV = {
+  PORT: process.env.PORT || "",
+  MONGO_URI: process.env.MONGO_URI || "",
+  JWT_SECRETWORD: process.env.JWT_SECRETWORD || "",
+CLOUDFLARE_ACCOUNT_ID: process.env.CLOUDFLARE_ACCOUNT_ID || "",
+  R2_ACCESS_KEY_ID: process.env.R2_ACCES_KEY_ID || "",
+  R2_SECRET_ACCESS: process.env.R2_SECRET_ACCESS || "",
+  R2_BUCKET_NAME: process.env.R2_BUCKET_NAME || "",
+  R2_PUBLIC_URL: process.env. R2_PUBLIC_URL || "",
+};
