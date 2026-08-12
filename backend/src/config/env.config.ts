7a0299220ee66d38953eeaa356d5d209564a74c3
Skomode <lejobass02@gmail.com>
2026-08-12 16:18:17 +0200

    Creacion de r2 cloudflareService

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -17,9 +17,9 @@ export const ENV: IENV = {
   PORT: process.env.PORT || "",
   MONGO_URI: process.env.MONGO_URI || "",
   JWT_SECRETWORD: process.env.JWT_SECRETWORD || "",
-CLOUDFLARE_ACCOUNT_ID: process.env.CLOUDFLARE_ACCOUNT_ID || "",
+  CLOUDFLARE_ACCOUNT_ID: process.env.CLOUDFLARE_ACCOUNT_ID || "",
   R2_ACCESS_KEY_ID: process.env.R2_ACCES_KEY_ID || "",
   R2_SECRET_ACCESS: process.env.R2_SECRET_ACCESS || "",
   R2_BUCKET_NAME: process.env.R2_BUCKET_NAME || "",
-  R2_PUBLIC_URL: process.env. R2_PUBLIC_URL || "",
+  R2_PUBLIC_URL: process.env.R2_PUBLIC_URL || "",
 };
---
+++ backend/src/services/r2Service.ts
@@ -0,0 +1,36 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { ENV } from "../config/env.config";
+import crypto from "crypto";
+
+const r2Client = new S3Client({
+  region: "auto",
+  endpoint: `https://${ENV.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  credentials: {
+    accessKeyId: ENV.R2_ACCESS_KEY_ID,
+    secretAccessKey: ENV.R2_SECRET_ACCESS,
+  },
+});
+
+export const generateUploadPresignedUrl = async (
+  folder: "covers" | "audio",
+  fileType: string
+) => {
+  const extension = fileType.split("/")[1] || "bin";
+  const fileName = `${folder}/${crypto.randomUUID()}.${extension}`;
+
+  const command = new PutObjectCommand({
+    Bucket: ENV.R2_BUCKET_NAME,
+    Key: fileName,
+    ContentType: fileType,
+  });
+
+  // URL firmada temporal para que el cliente suba el archivo (expira en 3 minutos)
+  const uploadUrl = await getSignedUrl(r2Client, command, { expiresIn: 180 });
+
+  // URL pública final para guardar en la base de datos
+  const publicUrl = `${ENV.R2_PUBLIC_URL}/${fileName}`;
+
+  return { uploadUrl, publicUrl };
+
+};
