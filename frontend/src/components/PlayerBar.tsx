9fbbf5c37e7dc19561a29ccc2367db2ba2696858
Skomode <lejobass02@gmail.com>
2026-09-08 15:09:00 +0200

    continuacion de la creacion del PlayerBar para la interfaz de usuario, importacion del state PlayerBar para el desarrollo de los botones  en progreso...

--- frontend/src/components/PlayerBar.tsx
+++ frontend/src/components/PlayerBar.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import { usePlayerStore } from '../store/usePlayerStore'
+
 
 const PlayerBar = () => {
+
+  const {currentSong, isPlaying, volume, isMuted, progress, togglePlay, toggleVolume, setVolume, setProgress } = usePlayerStore();
+
   return (
-    <div>
-      <button>Boton 1</button>
-    </div>
+    <footer>
+      
+      <div>
+        {currentSong ? (
+        <>
+          <img src={currentSong.pictureUrl} />
+          <div>
+          </div>
+        </>
+        ) : (<p>Selecciona una cancion</p>)}
+
+      </div>
+
+
+      <div>
+      <button>previous</button>
+      </div>
+      <div>
+      <button>Play</button>
+      </div>
+      <div>
+      <button>next</button>
+      </div>
+      <div>
+      <button>Repeat</button>
+      </div>
+      <div>
+      <button>Volume</button>
+      </div>
+
+    </footer>
   )
 }
 
