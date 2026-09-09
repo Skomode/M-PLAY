db18dcefac050a84ff83a4e326c4b2ac21ae128a
Skomode <lejobass02@gmail.com>
2026-09-09 21:58:19 +0200

    importacion e integracion en ruta de la pantalla 'Auth'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
-import "./App.css";
+
 import { BrowserRouter, Route, Routes } from "react-router";
 import PlayerBar from "./components/PlayerBar";
+import Auth from "./views/Auth";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PlayerBar />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   );
